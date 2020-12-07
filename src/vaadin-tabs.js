35c5ad3e8b3fef9bf3da9efaafe8c876ed2d7031
web-padawan <iamkulykov@gmail.com>
2020-12-07 14:57:37 +0200

    Update copyright headers

--- src/vaadin-tabs.js
+++ src/vaadin-tabs.js
@@ -1,8 +1,8 @@
 /**
-@license
-Copyright (c) 2017 Vaadin Ltd.
-This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
-*/
+ * @license
+ * Copyright (c) 2020 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
